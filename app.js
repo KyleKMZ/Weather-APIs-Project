14056dcc2f75cfd4604557c37bc77d3e5f07950e
Kaung Myat Zaw <zaw@purdue.edu>
2021-05-21 05:33:33 +0630

    Integrating forcasting and geocoding functionalities and some refactoring

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
+const process = require('process')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-geocode('Gaza', (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
+if (process.argv.length < 3) return console.log('Please provide location as argument!')
+
+const location = process.argv[2]
+geocode(location, (error, {latitude, longitude, location} = {}) => {
+    if (error) {
+        return console.log(error)
+    } 
+    forecast(latitude, longitude, (error, forecastData) => {
+        if (error) {
+            return console.log(error)
+        }
+
+        console.log(location)
+        console.log(forecastData)
+    })
 })
 
-forecast(-75.7088, 44.1545, (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-})
--- utils/forecast.js
+++ utils/forecast.js
@@ -1,21 +1,17 @@
 const request = require('postman-request')
 
-const forecast = (longitude, latitude, callback) => {
+const forecast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=458ead5a4c267ba0d99631ea27f6a749&query=' + latitude + ',' + longitude + '&units=f'
-    request({ url: url, json: true }, (error, response) => {
+    request({ url, json: true }, (error, {body} ) => {
         if (error) {
-            callback('HTTP Request Error!', undefined)
-        } else if (response.body.error) {
-            callback('Location Not Found!', undefined)
+            callback('Forecast: HTTP Request Error!', undefined)
+        } else if (body.error) {
+            callback('Forecast: Location Not Found!', undefined)
         } else {
-            const temperature = response.body.current.temperature 
-            const feelslike = response.body.current.feelslike
-            const description = response.body.current.weather_descriptions[0]
-            callback(undefined, {
-                temperature: temperature,
-                feelslike: feelslike,
-                description: description
-            })
+            const temperature = body.current.temperature 
+            const feelslike = body.current.feelslike
+            const description = body.current.weather_descriptions[0]
+            callback(undefined, description + ' throughout the day. The temperatue is ' + temperature + ' and feels like ' + feelslike + '.')
         }
     })
     
