634fe188f27808b6994bd0690c7110bb680a047b
Kaung Myat Zaw <zaw@purdue.edu>
2021-05-21 01:37:26 +0630

    Refactoring geocoding functionality into utils folder

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const request = require('postman-request')
+const geocode = require('./utils/geocode')
 
 // const url = 'http://api.weatherstack.com/current?access_key=458ead5a4c267ba0d99631ea27f6a749&query=51.5098,-0.1180'
 // request({ url: url, json: true }, (error, response) => {
 //     if (error) {
 //         console.log('HTTP Request Error!')
 //     } else if (response.body.error) {
-//         console.log('Location Not Found!)
+//         console.log('Location Not Found!')
 //     } else {
 //         const temperature = response.body.current.temperature 
 //         const feelslike = response.body.current.feelslike
@@ -14,13 +14,19 @@ const request = require('postman-request')
 //     }
 // })
 
-const geocode_url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Yangon.json?access_token=pk.eyJ1Ijoia3lsZXphdyIsImEiOiJja293d3pwbGEwMGk0MnZsbHdmY3dyemVuIn0.ODji_1yxKnPVTbCCzJ5Elw'
-request({ url: geocode_url, json: true }, (error, response) => {
-    if (error) {
-        console.log('HTTP Request failed!')
-    } else if (response.body.features.length === 0) {
-        console.log('Location Not Found!')
-    } else {
-        console.log(response.body.features[0].center)
-    }
+// const geocode_url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Yangon.json?access_token=pk.eyJ1Ijoia3lsZXphdyIsImEiOiJja293d3pwbGEwMGk0MnZsbHdmY3dyemVuIn0.ODji_1yxKnPVTbCCzJ5Elw'
+// request({ url: geocode_url, json: true }, (error, response) => {
+//     if (error) {
+//         console.log('HTTP Request failed!')
+//     } else if (response.body.features.length === 0) {
+//         console.log('Location Not Found!')
+//     } else {
+//         console.log(response.body.features[0].center)
+//     }
+// })
+
+
+geocode('Philadelphia New York', (error, data) => {
+    console.log('Error', error)
+    console.log('Data', data)
 })
