cdb7dfcce087cb054999af94effe86c3090f393e
Kaung Myat Zaw <zaw@purdue.edu>
2021-05-24 02:20:24 +0630

    Setting up express and empty endpoints; installing hbs for templating engine and setting up views

--- src/app.js
+++ src/app.js
@@ -1,21 +1,47 @@
 const process = require('process')
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-if (process.argv.length < 3) return console.log('Please provide location as argument!')
+const port = process.env.port || 3000
 
-const location = process.argv[2]
-geocode(location, (error, {latitude, longitude, location} = {}) => {
-    if (error) {
-        return console.log(error)
-    } 
-    forecast(latitude, longitude, (error, forecastData) => {
-        if (error) {
-            return console.log(error)
-        }
+server = express()
 
-        console.log(location)
-        console.log(forecastData)
-    })
+// Configuring express paths and hbs view engine
+const publicPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../views')
+server.use(express.static(publicPath))
+server.set('view engine', 'hbs')
+server.set('views', viewsPath)
+
+
+server.get('', (req, res) => {
+
 })
+
+server.get('/about.html', (req, res) => {
+    
+})
+
+server.get('/weather', (req, res) => {
+    // Need to rewrite this after frontend is done
+    // geocode(location, (error, {latitude, longitude, location} = {}) => {
+    //     if (error) {
+    //         return console.log(error)
+    //     } 
+    //     forecast(latitude, longitude, (error, forecastData) => {
+    //         if (error) {
+    //             return console.log(error)
+    //         }
+    
+    //         console.log(location)
+    //         console.log(forecastData)
+    //     })
+    // })
+})
+
+
+
 
