599b0a20eb2626adae946de1537378edd59d6f72
Kaung Myat Zaw <zaw@purdue.edu>
2021-05-26 22:56:34 +0630

    Continued wiring up API data to frontend

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,6 @@
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
+const loading_error = document.querySelector('#loading-error')
 const location_data = document.querySelector('#location')
 const description = document.querySelector('#description')
 const fahrenheit_temp = document.querySelector('#fahrenheit_temp')
@@ -18,13 +19,29 @@ const w_direction = document.querySelector('#w_direction')
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
+    loading_error.textContent = 'Loading...'
     const searchTerm = search.value
     fetch('/weather?address=' + searchTerm).then((response) => {
         response.json().then((data) => {
             if (data.error) {
-
+                loading_error.textContent = data.error
             } else {
-                
+                location_data.textContent = data.location
+                description.textContent = data.description
+                fahrenheit_temp.textContent = data.fahrenheit_temp
+                fahrenheit_feels.textContent = data.fahrenheit_feels
+                celsius_temp.textContent = data.celsius_temp
+                celsius_feels.textContent = data.celsius_feels
+                kelvin_temp.textContent = data.kelvin_temp
+                kelvin_feels.textContent = data.kelvin_feels
+                humidity.textContent = data.humidity
+                precipitation.textContent = data.precipitation
+                pressure.textContent = data.pressure
+                w_speed.textContent = data.w_speed
+                w_degree.textContent = data.w_degree
+                w_direction.textContent = data.w_direction
+
+                loading_error.textContent = ''
             }
         })
     })
